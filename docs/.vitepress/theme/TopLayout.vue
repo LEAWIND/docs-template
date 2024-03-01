d970bf0a345a3562b87cc3429ccb03cc9059beee
leawind <leawind@yeah.net>
2024-03-01 16:42:48 +0800

    Update TopLayout.vue

--- docs/.vitepress/theme/TopLayout.vue
+++ docs/.vitepress/theme/TopLayout.vue
@@ -1,11 +1,15 @@
 <script setup>
+
+
 import { useData } from 'vitepress';
 globalThis.useData = useData;
 
 import AutoLang from './scripts/AutoLang.mts';
 import './scripts/AutoSearch.mts';
 
-AutoLang();
+if (globalThis.window) {
+	AutoLang();
+}
 
 </script>
 
